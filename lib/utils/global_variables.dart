98fe7abbd63200b28d0ea1cc69caf016ccd42c77
jagriti <im.jagriti.jha@gmail.com>
2024-04-01 23:32:11 +0530

    updated with image picker,reset fields functionalities & validation

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,19 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class GlobalVars {
-  String? name = '',
+  String?
+      name = '',
       email = '',
       phone = '',
       addressOne = '',
       addressTwo = '',
       addressThree = '';
+
+  File?
+      image;
+
   GlobalVars._();
 
-  static final GlobalVars globals = GlobalVars._();
+  static final GlobalVars
+      globals =
+      GlobalVars._();
 
-  void reset() {
-    name = email = phone = addressOne = addressTwo = addressThree = null;
+  void
+      reset() {
+    name =
+        email = phone = addressOne = addressTwo = addressThree = null;
   }
 }
 
-Color primaryColor = Colors.blue.shade800;
+Color primaryColor = Colors
+    .blue
+    .shade800;
--- lib/views/resume_workspace.dart
+++ lib/views/resume_workspace.dart
@@ -1,27 +1,40 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:resume_builder/common_widgets/app_bar.dart';
 import 'package:resume_builder/common_widgets/tab_widget.dart';
 import 'package:resume_builder/common_widgets/text_field_widget.dart';
 import 'package:resume_builder/common_widgets/text_widget.dart';
 import 'package:resume_builder/utils/global_variables.dart';
 
-class ResumeWorkspace extends StatefulWidget {
-  const ResumeWorkspace({super.key});
+class ResumeWorkspace
+    extends StatefulWidget {
+  const ResumeWorkspace(
+      {super.key});
 
   @override
-  State<ResumeWorkspace> createState() => _ResumeWorkspaceState();
+  State<ResumeWorkspace> createState() =>
+      _ResumeWorkspaceState();
 }
 
-class _ResumeWorkspaceState extends State<ResumeWorkspace> {
-  final _formKey = GlobalKey<FormState>();
+class _ResumeWorkspaceState
+    extends State<
+        ResumeWorkspace> {
+  final _formKey =
+      GlobalKey<FormState>();
   @override
-  void dispose() {
+  void
+      dispose() {
     // TODO: implement dispose
-    super.dispose();
+    super
+        .dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+      BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: CustomAppBar.appBar(
@@ -71,8 +84,7 @@ class _ResumeWorkspaceState extends State<ResumeWorkspace> {
                         horizontal: 20,
                         vertical: 20,
                       ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 18, vertical: 20),
+                      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 14),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         boxShadow: [
@@ -83,98 +95,101 @@ class _ResumeWorkspaceState extends State<ResumeWorkspace> {
                             spreadRadius: 10,
                           )
                         ],
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(12)),
+                        borderRadius: const BorderRadius.all(Radius.circular(12)),
                       ),
                       child: Column(
                         children: [
                           Form(
                             key: _formKey,
-                            child: TextFieldWidget(
-                              icon: Icons.person_outline_rounded,
-                              hint: 'Enter your name',
-                              labelText: 'Name',
-                              validatorVariable: GlobalVars.globals.name,
-                              validatorFunc: (val) {
-                                return val!.isEmpty ? 'enter name' : null;
-                              },
-                              saveData: (val) {
-                                GlobalVars.globals.name = val;
-                              },
+                            child: Column(
+                              children: [
+                                TextFieldWidget(
+                                  icon: Icons.person_outline_rounded,
+                                  hint: 'Enter your name',
+                                  labelText: 'Name',
+                                  validatorVariable: GlobalVars.globals.name,
+                                  validatorFunc: (val) {
+                                    return val!.isEmpty ? 'enter name' : null;
+                                  },
+                                  saveData: (val) {
+                                    GlobalVars.globals.name = val;
+                                  },
+                                ),
+                                TextFieldWidget(
+                                  icon: Icons.email_outlined,
+                                  hint: 'Enter your email',
+                                  labelText: 'Email',
+                                  validatorVariable: GlobalVars.globals.email,
+                                  validatorFunc: (val) {
+                                    return val!.isEmpty ? 'enter email' : null;
+                                  },
+                                  saveData: (val) {
+                                    GlobalVars.globals.email = val;
+                                  },
+                                ),
+                                TextFieldWidget(
+                                  icon: Icons.phone,
+                                  hint: 'Enter your phone number',
+                                  labelText: 'Phone',
+                                  validatorVariable: GlobalVars.globals.phone,
+                                  validatorFunc: (val) {
+                                    return val!.isEmpty ? 'enter phone number' : null;
+                                  },
+                                  saveData: (val) {
+                                    GlobalVars.globals.phone = val;
+                                  },
+                                ),
+                                TextFieldWidget(
+                                  icon: Icons.location_on_outlined,
+                                  hint: 'Enter your address',
+                                  labelText: 'Address',
+                                  validatorVariable: GlobalVars.globals.addressOne,
+                                  validatorFunc: (val) {
+                                    return val!.isEmpty ? 'enter address' : null;
+                                  },
+                                  saveData: (val) {
+                                    GlobalVars.globals.addressOne = val;
+                                  },
+                                ),
+                                TextFieldWidget(
+                                  hint: 'Enter your address',
+                                  labelText: 'Address 1',
+                                  validatorVariable: GlobalVars.globals.addressTwo,
+                                  validatorFunc: (val) {
+                                    return val!.isEmpty ? 'enter address' : null;
+                                  },
+                                  saveData: (val) {
+                                    GlobalVars.globals.addressTwo = val;
+                                  },
+                                ),
+                                TextFieldWidget(
+                                  hint: 'Enter your address',
+                                  labelText: 'Address 2',
+                                  validatorVariable: GlobalVars.globals.addressThree,
+                                  validatorFunc: (val) {
+                                    return val!.isEmpty ? 'enter address' : null;
+                                  },
+                                  saveData: (val) {
+                                    GlobalVars.globals.addressThree = val;
+                                  },
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      GlobalVars.globals.reset();
+                                      _formKey.currentState!.reset();
+                                    },
+                                    child: TextWidget(
+                                      title: 'Rest',
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: primaryColor,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          TextFieldWidget(
-                            icon: Icons.email_outlined,
-                            hint: 'Enter your email',
-                            labelText: 'Email',
-                            validatorVariable: GlobalVars.globals.email,
-                            validatorFunc: (val) {
-                              return val!.isEmpty ? 'enter email' : null;
-                            },
-                            saveData: (val) {
-                              GlobalVars.globals.email = val;
-                            },
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          TextFieldWidget(
-                            icon: Icons.phone,
-                            hint: 'Enter your phone number',
-                            labelText: 'Phone',
-                            validatorVariable: GlobalVars.globals.phone,
-                            validatorFunc: (val) {
-                              return val!.isEmpty ? 'enter phone number' : null;
-                            },
-                            saveData: (val) {
-                              GlobalVars.globals.phone = val;
-                            },
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          TextFieldWidget(
-                            icon: Icons.location_on_outlined,
-                            hint: 'Enter your address',
-                            labelText: 'Address',
-                            validatorVariable: GlobalVars.globals.addressOne,
-                            validatorFunc: (val) {
-                              return val!.isEmpty ? 'enter address' : null;
-                            },
-                            saveData: (val) {
-                              GlobalVars.globals.addressOne = val;
-                            },
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          TextFieldWidget(
-                            hint: 'Enter your address',
-                            labelText: 'Address 1',
-                            validatorVariable: GlobalVars.globals.addressTwo,
-                            validatorFunc: (val) {
-                              return val!.isEmpty ? 'enter address' : null;
-                            },
-                            saveData: (val) {
-                              GlobalVars.globals.addressTwo = val;
-                            },
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          TextFieldWidget(
-                            hint: 'Enter your address',
-                            labelText: 'Address 2',
-                            validatorVariable: GlobalVars.globals.addressThree,
-                            validatorFunc: (val) {
-                              return val!.isEmpty ? 'enter address' : null;
-                            },
-                            saveData: (val) {
-                              GlobalVars.globals.addressThree = val;
-                            },
                           ),
                         ],
                       ),
@@ -184,17 +199,17 @@ class _ResumeWorkspaceState extends State<ResumeWorkspace> {
                         bool validation = _formKey.currentState!.validate();
                         if (validation) {
                           _formKey.currentState!.save();
+                          TabWidget.index = 1;
+                          setState(() {});
                         }
                         ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(
                             content: Text(
-                              validation
-                                  ? 'Form saved'
-                                  : 'Please enter the required field.',
+                              validation ? 'Form saved' : 'Please enter the required field.',
                             ),
                           ),
                         );
-                        print(GlobalVars.globals.name);
+                        // print(GlobalVars.globals.name);
                       },
                       style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
@@ -215,14 +230,13 @@ class _ResumeWorkspaceState extends State<ResumeWorkspace> {
                   ],
                 ),
                 Container(
-                  height: 500,
+                  height: 300,
                   width: double.infinity,
                   margin: const EdgeInsets.symmetric(
                     horizontal: 20,
                     vertical: 20,
                   ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+                  padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
                   decoration: BoxDecoration(
                     color: Colors.white,
                     boxShadow: [
@@ -235,7 +249,48 @@ class _ResumeWorkspaceState extends State<ResumeWorkspace> {
                     ],
                     borderRadius: const BorderRadius.all(Radius.circular(12)),
                   ),
-                  child: Text('hello'),
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundColor: Colors.grey.shade300,
+                          foregroundImage: GlobalVars.globals.image != null
+                              ? FileImage(
+                                  GlobalVars.globals.image!,
+                                )
+                              : null,
+                          child: TextWidget(
+                            title: 'Add',
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: CircleAvatar(
+                            backgroundColor: primaryColor,
+                            child: IconButton(
+                              icon: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                              onPressed: () async {
+                                ImagePicker image = ImagePicker();
+                                XFile? file = await image.pickImage(source: ImageSource.gallery);
+                                if (file != null) {
+                                  GlobalVars.globals.image = File(file.path);
+                                  setState(() {});
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 )
               ],
             ),
